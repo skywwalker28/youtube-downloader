1981054ede5c0082638e7226458878801afea605
skywwalker28 <wsky3478@gmail.com>
2026-03-19 16:02:07 +0300

    Delete src/main directory
    delete

--- src/main/java/org/example/youtubedownloader/dto/YoutubeInfoRequest.java
+++
@@ -1,8 +0,0 @@
-package org.example.youtubedownloader.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record YoutubeInfoRequest(
-        @NotBlank(message = "url must not be blank")
-        String url
-) {}
--- src/main/java/org/example/youtubedownloader/service/YoutubeService.java
+++
@@ -1,8 +0,0 @@
-package org.example.youtubedownloader.service;
-
-import org.example.youtubedownloader.dto.YoutubeInfoRequest;
-import org.example.youtubedownloader.dto.YoutubeInfoResponse;
-
-public interface YoutubeService {
-    YoutubeInfoResponse getVideoInfo(YoutubeInfoRequest request);
-}
